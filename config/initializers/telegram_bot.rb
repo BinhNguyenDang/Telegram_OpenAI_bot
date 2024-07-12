3b3379b2fa13eb91e8c26fa725a5cc6606ba7192
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-07-13 00:06:07 +0700

    Tested

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -1,37 +1,31 @@
 require 'telegram/bot'
 
-Rails.application.config.after_initialize do 
-    token = Rails.application.credentials.dig(:telegram_bot)
+Rails.application.config.after_initialize do
+  puts "Running Telegram Bot Job!"
+  TelegramBotWorkerJob.perform_async(Rails.application.credentials.dig(:telegram_bot))
+end
 
-    Telegram::Bot::Client.run(token) do |bot|
-        Rails.application.config.telegram_bot = bot
+Rails.application.config.after_initialize do
+  token = Rails.application.credentials.dig(:telegram_bot)
 
-        bot.listen do |message|
-            next unless message.is_a?(Telegram::Bot::Types::Message)
-            case message.text
-            when '/start'
-                bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} | Chat ID: #{message.chat.id}")
-            when '/stop'
-                bot.api.send_message(chat_id: message.chat.id, text: "Goodbye, #{message.from.first_name} | Chat ID: #{message.chat.id}")
-            end
-        end
-    end
-
+  Telegram::Bot::Client.run(token) do |bot|
+    Rails.application.config.telegram_bot = bot
+    bot.api.get_updates(offset: -1)
+  end
 rescue Telegram::Bot::Exceptions::ResponseError => e
-    Rails.logger.error e
-    Rails.application.config.telegram_bot.stop
-    Rails.application.config.telegram_bot.api.delete_webhook
+  Rails.logger.error e
+  Rails.application.config.telegram_bot.stop
+  Rails.application.config.telegram_bot.api.delete_webhook
 end
 
 Signal.trap("TERM") do
-    puts "Shutting down bot..."
-    Rails.application.config.telegram_bot.stop
-    exit
+  puts "Shutting down bot..."
+  Rails.application.config.telegram_bot.stop
+  exit
 end
 
 Signal.trap("INT") do
-    puts "Shutting down bot..."
-    Rails.application.config.telegram_bot.stop
-    exit
+  puts "Shutting down bot..."
+  Rails.application.config.telegram_bot.stop
+  exit
 end
-
